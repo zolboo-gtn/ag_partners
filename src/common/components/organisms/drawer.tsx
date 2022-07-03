96123bdb7d6a43215dc0465e6a85cdde5bc73d88
zolboo-gtn <e.zolboo@gtn.co.jp>
2022-07-04 00:24:24 +0800

    drawer

--- src/common/components/organisms/drawer.tsx
+++ src/common/components/organisms/drawer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import type { FCC } from "react";
 
 export const id = "my-drawer";
@@ -5,20 +6,19 @@ export const Drawer: FCC = ({ children }) => {
   return (
     <div className="drawer">
       <input id={id} type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
-        {children}
-        {/* <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
-          Open drawer
-        </label> */}
-      </div>
+      <div className="drawer-content">{children}</div>
       <div className="drawer-side">
-        <label htmlFor={id} className="drawer-overlay"></label>
+        <label htmlFor={id} className="drawer-overlay" />
         <ul className="menu w-80 overflow-y-auto bg-base-100 p-4 text-base-content">
           <li>
-            <a>Sidebar Item 1</a>
+            <Link href="/">
+              <a>Sidebar Item 1</a>
+            </Link>
           </li>
           <li>
-            <a>Sidebar Item 2</a>
+            <Link href="/">
+              <a>Sidebar Item 2</a>
+            </Link>
           </li>
         </ul>
       </div>
